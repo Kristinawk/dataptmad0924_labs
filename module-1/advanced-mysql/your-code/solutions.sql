8903e0329c39174440646cc7b0265fc4b8cd42ea
unknown <kristina_ukr_sp@hotmail.es>
2024-12-11 18:18:22 +0100

    lab finished

--- module-1/advanced-mysql/your-code/solutions.sql
+++ module-1/advanced-mysql/your-code/solutions.sql
@@ -95,3 +95,43 @@ FROM total_royalty_and_advance
 GROUP BY au_id
 ORDER BY Profits DESC
 LIMIT 3;
+
+
+
+-- Challenge 3
+
+CREATE TABLE most_profiting_authors (
+			au_id INT,
+    		profits DECIMAL(10, 2));
+    		
+-- Step 1: Create a temporary table to calculate royalties and advances
+CREATE TEMPORARY TABLE royalty_and_advance AS
+SELECT
+        titles.title_id,
+        authors.au_id,
+        titles.advance * titleauthor.royaltyper / 100 AS [Advance],
+        titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 AS [Sales_royality]
+FROM authors
+        INNER JOIN titleauthor ON titleauthor.au_id = authors.au_id
+        INNER JOIN titles ON titles.title_id = titleauthor.title_id
+        INNER JOIN sales ON sales.title_id = titleauthor.title_id;
+
+-- Step 2: Create a temporary table to calculate the total advance and royalties
+CREATE TEMPORARY TABLE total_royalty_and_advance AS
+SELECT
+        title_id,
+        au_id,
+        SUM(Advance) AS [Total_Advance],
+        SUM(Sales_royality) AS [Total_Sales_Royalties]
+FROM royalty_and_advance
+GROUP BY title_id, au_id;
+
+-- Step 3: Insert the top 3 profiting authors into the permanent table
+INSERT INTO most_profiting_authors (au_id, profits)
+SELECT
+        au_id,
+        SUM(Total_Advance + Total_Sales_Royalties) AS Profits
+FROM total_royalty_and_advance
+GROUP BY au_id
+ORDER BY Profits DESC
+LIMIT 3;
